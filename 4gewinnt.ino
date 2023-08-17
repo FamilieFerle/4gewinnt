d3748ad2820f77f96ee329a6dc2d0c5505a355f1
Anja Ferle <Anja.Ferle@gmx.de>
2023-08-17 14:48:12 +0200

    Victory added

--- 4gewinnt.ino
+++ 4gewinnt.ino
@@ -1,9 +1,11 @@
 #include <Adafruit_NeoPixel.h>
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define LED_PIN     1
+#define LED_PIN 1
 // How many NeoPixels are attached to the Arduino?
-#define LED_COUNT  42
+#define LED_COUNT 42
+#define ROW_COUNT 6
+#define COL_COUNT 7
 
 #define BUTTON_1 2
 #define BUTTON_2 3
@@ -17,20 +19,16 @@
 #define LED_G 10
 #define LED_B 11
 
-bool isPlayerRed = true;
-uint32_t colorPlayerRed = 0xFF0000; //Red
-uint32_t colorPlayerYel = 0xFFFF00; //Yellow
-
 // NeoPixel brightness, 0 (min) to 255 (max)
-#define BRIGHTNESS 50 // Set BRIGHTNESS to about 1/5 (max = 255)
+#define BRIGHTNESS 50  // Set BRIGHTNESS to about 1/5 (max = 255)
 
-int column1[6] = {35, 34, 21, 20, 7, 6};
-int column2[6] = {36, 33, 22, 19, 8, 5};
-int column3[6] = {37, 32, 23, 18, 9, 4};
-int column4[6] = {38, 31, 24, 17, 10, 3};
-int column5[6] = {39, 30, 25, 16, 11, 2};
-int column6[6] = {40, 29, 26, 15, 12, 1};
-int column7[6] = {41, 28, 27, 14, 13, 0};
+int columns[7][6] = { { 35, 34, 21, 20, 7, 6 },
+                      { 36, 33, 22, 19, 8, 5 },
+                      { 37, 32, 23, 18, 9, 4 },
+                      { 38, 31, 24, 17, 10, 3 },
+                      { 39, 30, 25, 16, 11, 2 },
+                      { 40, 29, 26, 15, 12, 1 },
+                      { 41, 28, 27, 14, 13, 0 } };
 
 // Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
@@ -43,72 +41,114 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 
+bool isPlayerRed = true;
+uint32_t red = strip.Color(255, 0, 0);
+uint32_t yellow = strip.Color(255, 255, 0);
+uint32_t background = strip.Color(20, 20, 20);
+
 void setup() {
-  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            // Turn OFF all pixels ASAP
+  strip.begin();  // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();   // Turn OFF all pixels ASAP
   strip.setBrightness(BRIGHTNESS);
-  
-  colorWipe(strip.Color(20,   20,   20), 0); // All light grey
+
+  colorWipe(background, 0);  // All light grey
   setLEDPlayerRed();
 }
 
 void loop() {
   int buttonPressed = -1;
-  if(isButtonPressed(buttonPressed))
-  {
-
+  if (isButtonPressed(buttonPressed)) {
+    //Starte in oberer Zeile und lasse den Farbpunkt nach unten fallen
+    int i = 0;
+    while (i < ROW_COUNT) {
+      int idx = columns[buttonPressed - 1][i];
+      auto currPixCol = strip.getPixelColor(idx);
+      if (currPixCol == background) {
+        strip.setPixelColor(idx, isPlayerRed ? red : yellow);
+        delay(200);
+        i++;
+      } else {
+        break;
+      }
+    }
+    checkForVictory();
+    changePlayer();
   }
 }
 
-bool isButtonPressed(int& f_buttonPressed)
-{
-  if(digitalRead(BUTTON_1) == HIGH)
-  {
+void checkForVictory() {
+  //vertical
+  for (int col = 0; col < COL_COUNT; col++) {
+    int beforeCol = strip.getPixelColor(columns[col][0]);
+    int counter = 1;
+    for (int row = 1; row < ROW_COUNT; row++) {
+      int curCol = strip.getPixelColor(columns[col][row]);
+      if (curCol != background) {
+        if (beforeCol == curCol) {
+          counter++;
+        } else {
+          counter = 1;
+        }
+        beforeCol = curCol;
+      }
+      if (counter == 4) {
+        gameWon();
+      }
+    }
+  }
+}
+
+void gameWon() {
+  while (true) {
+    colorWipe(isPlayerRed ? red : yellow, 200);
+    colorWipe(0, 200);
+  }
+}
+
+void changePlayer() {
+  isPlayerRed = !isPlayerRed;
+  isPlayerRed ? setLEDPlayerRed() : setLEDPlayerYel();
+}
+
+bool isButtonPressed(int& f_buttonPressed) {
+  if (digitalRead(BUTTON_1) == HIGH) {
     f_buttonPressed = 1;
     return true;
   }
-  if(digitalRead(BUTTON_2) == HIGH)
-  {
+  if (digitalRead(BUTTON_2) == HIGH) {
     f_buttonPressed = 2;
     return true;
   }
-  if(digitalRead(BUTTON_3) == HIGH)
-  {
+  if (digitalRead(BUTTON_3) == HIGH) {
     f_buttonPressed = 3;
     return true;
   }
-  if(digitalRead(BUTTON_4) == HIGH)
-  {
+  if (digitalRead(BUTTON_4) == HIGH) {
     f_buttonPressed = 4;
     return true;
   }
-  if(digitalRead(BUTTON_5) == HIGH)
-  {
+  if (digitalRead(BUTTON_5) == HIGH) {
     f_buttonPressed = 5;
     return true;
   }
-  if(digitalRead(BUTTON_6) == HIGH)
-  {
+  if (digitalRead(BUTTON_6) == HIGH) {
     f_buttonPressed = 6;
     return true;
   }
-  if(digitalRead(BUTTON_7) == HIGH)
-  {
+  if (digitalRead(BUTTON_7) == HIGH) {
     f_buttonPressed = 7;
     return true;
   }
   return false;
 }
 
-void setLEDPlayerRed()
-{
+void setLEDPlayerRed() {
   digitalWrite(LED_R, HIGH);
   digitalWrite(LED_G, LOW);
   digitalWrite(LED_B, LOW);
 }
 
-void setLEDPlayerYel()
-{
+void setLEDPlayerYel() {
   digitalWrite(LED_R, HIGH);
   digitalWrite(LED_G, HIGH);
   digitalWrite(LED_B, LOW);
@@ -120,10 +160,9 @@ void setLEDPlayerYel()
 // strip.Color(red, green, blue) as shown in the loop() function above),
 // and a delay time (in milliseconds) between pixels.
 void colorWipe(uint32_t color, int wait) {
-  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
-    strip.show();                          //  Update strip to match
-    delay(wait);                           //  Pause for a moment
+  for (int i = 0; i < strip.numPixels(); i++) {  // For each pixel in strip...
+    strip.setPixelColor(i, color);               //  Set pixel's color (in RAM)
+    strip.show();                                //  Update strip to match
+    delay(wait);                                 //  Pause for a moment
   }
 }
-
